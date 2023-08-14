89af2597c3046c6a9d0a9c81cb6d5090f67ec348
Oumar Yanni <oumaryanni@gmail.com>
2023-08-14 18:51:21 +0000

    Added todos display and updated style

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
 import "./style.css";
+
+
+const ul = document.querySelector("ul");
+
+const todos = [
+    {
+        text: "devenir un excellent développeur",
+        done: true
+    },
+    {
+        text: "Exceller en JavaScript",
+        done: false
+    }
+];
+
+
+const displayTodo = () => {
+const todosNode = todos.map((todo, index) => {
+    return createTodoElement(todo, index);
+});
+ul.innerHTML = "";
+ul.append(...todosNode);
+};
+
+
+const createTodoElement = (todo, index) => {
+    const li = document.createElement("li");
+    li.innerHTML = `
+    <span class="todo ${todo.done ? "done" : ""}"></span>
+    
+    <p>${todo.text}</p>`;
+    
+    return li;
+}
